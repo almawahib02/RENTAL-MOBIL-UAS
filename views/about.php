91bbe5d6f77f9b565331ab705adfb5bc0a36f0c9
bilkis38 <bilkisnisa38@gmail.com>
2023-05-29 12:43:39 +0700

    TAMBAH ABOUT.PHP

---
+++ views/about.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+    <title>RENTAL MOBIL</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="assets/img/logo.png" alt="Logo">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Catalog</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="views/login_menu.php">Login</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <div class="card">
+            <img src="assets/img/toyota-rush1.jpg" alt="Photo 1">
+            <h3>MOBIL 1</h3>
+            <p>Rp. 250000/H</p>
+            <button class="rent-button">Rent</button>
+        </div>
+        <div class="card">
+            <img src="assets/img/toyota-rush1.jpg" alt="Photo 2">
+            <h3>MOBIL 2</h3>
+            <p>Rp. 250000/H</p>
+            <button class="rent-button">Rent</button>
+        </div>
+        <div class="card">
+            <img src="assets/img/toyota-rush1.jpg" alt="Photo 3">
+            <h3>MOBIL 3</h3>
+            <p>Rp. 250000/H</p>
+            <button class="rent-button">Rent</button>
+        </div>
+    </main>
+</body>
+</html>
